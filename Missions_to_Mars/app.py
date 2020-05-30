e27c614b8bb075f81b65b5235d5522479d230a84
oianchevska <oianchevska@gmail.com>
2020-05-30 16:18:30 -0400

    HW-12: Minor fixes v.2

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,7 +1,6 @@
-from flask import Flask,render_template,redirect
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
-
 
 app = Flask(__name__)
 
@@ -14,19 +13,14 @@ def home():
     return render_template("index.html", info_m=mars_mission)
 
 
-
 @app.route("/scrape")
 def scrape():
     mars_data_app = scrape_mars.scrape()
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({},mars_data_app, upsert=True)
+    mongo.db.collection.update({}, mars_data_app, upsert=True)
     # Redirect back to home page
     return redirect("/")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    #mars_mission = mongo.db.collection.find_one()
-    #d=mars_mission.mars_facts
-    #d=mars_mission["mars_facts"]
-    #print(mars_mission)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -10,57 +10,57 @@ def init_browser():
 
 
 def scrape():
-    mars_info={}
+    mars_info = {}
+    browser = init_browser()
 
-    browser=init_browser()
-    #NASA Mars News
+    # NASA Mars News
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
-    time.sleep(5)
+    time.sleep(2)
     html = browser.html
     soup = bs(html, "html.parser")
     news_title = soup.find_all(attrs={'class': 'content_title'})[1].get_text()
     news_p = soup.find_all(attrs={'class': 'article_teaser_body'})[0].get_text()
-    mars_info['news_title']=news_title
-    mars_info['news_text']=news_p
 
-    #JPL Mars Space Images - Featured Image
+    mars_info['news_title'] = news_title
+    mars_info['news_text'] = news_p
+
+    # JPL Mars Space Images - Featured Image
     featured_image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(featured_image_url)
     browser.find_by_css('a#full_image.button.fancybox').first.click()
-    time.sleep(5)
+    time.sleep(1)
     html = browser.html
     soup = bs(html, "html.parser")
     mars_image = soup.find_all(attrs={'class': 'fancybox-image'})[0]
     url_image = mars_image.get('src')
     featured_image_url = 'https://www.jpl.nasa.gov/' + url_image
+
     mars_info['mars_url'] = featured_image_url
 
-    #Mars Weather
+    # Mars Weather
     twitter_url = 'https://twitter.com/marswxreport?lang=en'
     browser.visit(twitter_url)
-    time.sleep(5)
+    time.sleep(1)
     html = browser.html
     soup = bs(html, "html.parser")
     twitter_news = soup.find_all(attrs={'role': 'article'})[0]
     mars_weather = twitter_news.find(attrs={'lang': 'en'}).get_text()
+
     mars_info['mars_weather'] = mars_weather
 
-    #Mars Facts
+    # Mars Facts
     mars_url = 'https://space-facts.com/mars/'
     mars_table = pd.read_html(mars_url)[0]
-    mars_table = mars_table.rename(columns={0: 'Mars', 1: 'Value'})
-    mars_table = mars_table.set_index('Mars')
-    mars_data = mars_table.to_html(classes='mars_data')
-    #mars_data = mars_data.replace('\n', ' ')
+    mars_table = mars_table.rename(columns={0: 'Value', 1: 'Description'})
+    mars_table = mars_table.set_index('Description')
+    mars_data = mars_table.to_html(classes='mars_data', justify='left')
+
     mars_info['mars_facts'] = mars_data
 
-    #Mars Hemispheres
+    # Mars Hemispheres
     hem_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(hem_url)
-
-    # 'img.thumb'
-
     hemisphere_image_urls = []
     for i in range(len(browser.find_by_css('img.thumb'))):
         img_page = browser.find_by_css('img.thumb')
@@ -72,14 +72,13 @@ def scrape():
         title_info = soup.find_all("h2", attrs={'class': 'title'})[0].get_text()
         dict_info = {"title": title_info, "img_url": 'https://astrogeology.usgs.gov' + image_info}
         hemisphere_image_urls.append(dict_info)
-
         browser.back()
-    mars_info['mars_images_titles'] = hemisphere_image_urls
 
+    mars_info['mars_images_titles'] = hemisphere_image_urls
 
     browser.quit()
     return mars_info
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     scrape()
